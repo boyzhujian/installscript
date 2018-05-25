b0dc184d8b18068d2a2bb3365684963f261fc084
Damon <cisco.damon@gmail.com>
2018-05-25 10:47:59 +0800

    Update gencert.sh

--- cert/gencert.sh
+++ cert/gencert.sh
@@ -1,3 +1,5 @@
 openssl genrsa -out privkey.pem 1024
 openssl req -new -key privkey.pem -out certreq.csr
 openssl x509 -req -days  3650 -in certreq.csr -signkey privkey.pem -out newcert.pem
+
+#proxy -har -cert newcert.pem  -key privkey.pem -v 2 -addr :7090
