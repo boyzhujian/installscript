7998e7ac6cd6f97c8f264ac06cfc9f440dad9bd7
Damon <cisco.damon@gmail.com>
2018-05-25 11:04:09 +0800

    Update gencert.sh

--- cert/gencert.sh
+++ cert/gencert.sh
@@ -2,4 +2,5 @@ openssl genrsa -out privkey.pem 1024
 openssl req -new -key privkey.pem -out certreq.csr
 openssl x509 -req -days  3650 -in certreq.csr -signkey privkey.pem -out newcert.pem
 
+#even add in trust ,firefox still not work
 #proxy -har -cert newcert.pem  -key privkey.pem -v 2 -addr :7090
