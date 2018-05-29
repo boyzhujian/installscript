5b0730c63c094f9b381ec7d38a3c51d1fb829e6c
Damon <cisco.damon@gmail.com>
2018-05-29 16:27:37 +0800

    Update installk8.sh

--- coreos/installk8.sh
+++ coreos/installk8.sh
@@ -17,4 +17,10 @@ systemctl daemon-reload
 systemctl restart kubelet
 
 kubeadm  init --apiserver-advertise-address  172.17.8.101
+kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address  172.17.8.101
+
+
+
+
+ kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
