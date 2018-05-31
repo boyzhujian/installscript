c4e68e9c22619a97f2328cbb764641f6926997c4
Damon <cisco.damon@gmail.com>
2018-05-31 14:30:09 +0800

    Create install.sh

---
+++ kubenetes/kubeadm/centos7/install.sh
@@ -0,0 +1,31 @@
+#question to anserer
+#if docker version higher than kubeadm need ,is it ok?
+
+yum install -y docker  
+systemctl enable docker && systemctl start docker
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+setenforce 0
+yum install -y kubelet kubeadm kubectl
+systemctl enable kubelet && systemctl start kubelet
+
+
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+
+systemctl  stop  firewalld
+systemctl  disable  firewalld
+swapoff -a
