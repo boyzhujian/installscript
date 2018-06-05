91e6068928d4a9fdd4aa1696b8cd05005db54937
Damon <cisco.damon@gmail.com>
2018-06-05 10:17:43 +0800

    Update centosinstallkvm.sh

--- kvm/centosinstallkvm.sh
+++ kvm/centosinstallkvm.sh
@@ -6,10 +6,11 @@ systemctl enable libvirtd
 qemu-img create -f qcow2 mycentos7min 20G
 
 
-virt-install   
---name=mycentos7min   --vcpus=1   --ram 1024         
---disk path=/mnt/disk2/virshimg/mycentos7min,format=qcow2         --check-cpu   --accelerate   
---graphics  vnc    --os-type linux   --cdrom /mnt/disk2/iso/CentOS-7-x86_64-Minimal-1804.iso
+virt-install    \
+--name=mycentos7min   --vcpus=1   --ram 1024        \ 
+--disk path=/mnt/disk2/virshimg/mycentos7min,format=qcow2         --check-cpu   --accelerate   \
+--graphics  vnc    --os-type linux   --cdrom /mnt/disk2/iso/CentOS-7-x86_64-Minimal-1804.iso \
+--initrd-inject=/path/to/my.ks --extra-args "ks=file:/my.ks"
 
 
 
