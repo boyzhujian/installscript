d6912c843c7e5601303dbfd32c94cdb60871b527
Damon <cisco.damon@gmail.com>
2018-06-07 17:11:44 +0800

    Create savebody.py

---
+++ mitmproxy/sample/savebody.py
@@ -0,0 +1,12 @@
+def response(flow):
+    with open("file_to_write_data_to.txt", "ab") as ofile:
+        ofile.write(flow.request.pretty_url.encode())
+
+        if flow.request.content:
+            ofile.write(flow.request.content)
+
+        if flow.response.content:
+            ofile.write(flow.response.content)
+
+        # Add other separators etc. however you want
+        ofile.write(b"-------")
