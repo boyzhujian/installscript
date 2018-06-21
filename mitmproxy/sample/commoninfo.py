e42f0fabdf4c30b0153f0734bcd22dcd89307e33
Damon <cisco.damon@gmail.com>
2018-06-21 08:48:03 +0800

    Create commoninfo.py

---
+++ mitmproxy/sample/commoninfo.py
@@ -0,0 +1,26 @@
+from mitmproxy import ctx
+
+def request(flow):
+    request = flow.request
+    info = ctx.log.info
+    info(request.url)
+    info(str(request.headers))
+    info(str(request.cookies))
+    info(request.host)
+    info(request.method)
+    info(str(request.port))
+    info(request.scheme)
+
+
+
+
+def response(flow):
+    response = flow.response
+    info = ctx.log.info
+    info(str(response.status_code))
+    info(str(response.headers))
+    info(str(response.cookies))
+    info(str(response.text))
+    
+    
+  https://juejin.im/post/5ac9ea6d518825364001b5b9
