b0674e1d4c8f12cc849a5c6ea376719b2bf656c8
Damon <cisco.damon@gmail.com>
2018-07-06 09:36:44 +0800

    Update and rename forcentos7install to forcentos7install.sh

--- chrome/forcentos7install.sh
+++ chrome/forcentos7install.sh
@@ -4,5 +4,4 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 yum install epel-release
 yum install libappindicator-gtk3
 yum install lsb
-
 rpm -ivh google-chrome-stable_current_x86_64.rpm 
