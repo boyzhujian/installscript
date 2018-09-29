8ad9a2cacc8d4972fa10e0503a1078bc7f3670e3
Damon <cisco.damon@gmail.com>
2018-09-29 15:17:10 +0800

    Update installweave.sh

--- kubenetes/kubeadm/centos7/installweave.sh
+++ kubenetes/kubeadm/centos7/installweave.sh
@@ -1,2 +1,4 @@
 export kubever=$(kubectl version | base64 | tr -d '')
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
+
+#kubectl apply -f "https://cloud.weave.works/k8s/v1.10/net.yaml?k8s-version=1.12.0"
