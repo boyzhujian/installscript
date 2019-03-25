ef9bb8cf30421fdc808f8f7ace5fdfcb14b30dcc
Damon <610576334@qq.com>
2019-03-25 14:44:48 +0800

    Create download.sh

---
+++ etcd/clustersetup/download.sh
@@ -0,0 +1,4 @@
+curl -L http://10.252.52.191:8000/etcd/etcd -o /usr/local/bin/etcd
+curl -L http://10.252.52.191:8000/etcd/etcdctl -o /usr/local/bin/etcdctl
+chmod  u+x /usr/local/bin/etcd
+chmod  u+x /usr/local/bin/etcdctl
