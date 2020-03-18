94253a68c96038cd8a7553b251a6f31f5b8dbd28
Damon <610576334@qq.com>
2020-03-18 22:05:19 +0800

    Create vmran.bat

---
+++ esxi/hp/vmcmd/vmran.bat
@@ -0,0 +1,30 @@
+
+
+D:\vmware9\vmrun -T ws start "E:\vmimg\server2008r2\Windows Server 2008 R2 x64.vmx"  
+
+
+
+D:\vmware9\vmrun -T ws start "E:\vmimg\iredmailcentos\iredmail.vmx"  
+
+D:\vmware9\vmrun -T ws start "D:\vmimg\xpgroup\linked-1\Clone of shouhuiorigianlxp.vmx"  
+D:\vmware9\vmrun -T ws start "D:\vmimg\xpgroup\linked-shouhui2\Clone of shouhuiorigianlxp.vmx"  
+D:\vmware9\vmrun -T ws start "D:\vmimg\xpgroup\linked-shouhui3\Clone of shouhuiorigianlxp.vmx"  
+D:\vmware9\vmrun -T ws start "D:\vmimg\xpgroup\linked-shouhui11\Clone of shouhuiorigianlxp.vmx"  
+D:\vmware9\vmrun -T ws start "D:\vmimg\xpgroup\linked-shouhui12\Clone of shouhuiorigianlxp.vmx"  
+D:\vmware9\vmrun -T ws start "D:\vmimg\xpgroup\linked-shouhui13\shouhui13.vmx"  
+D:\vmware9\vmrun -T ws start "D:\vmimg\xpgroup\linked-shouhui14\shouhui14.vmx"  
+D:\vmware9\vmrun -T ws start "D:\vmimg\win7group\link-shouhuimainwin7\Clone of Windows 7.vmx"  
+
+D:\vmware9\vmrun -T ws start "I:\vmimg\win7-32bit\windows 7.vmx"  
+D:\vmware9\vmrun -T ws start "D:\vmimg\win7group\link-main\Clone of Windows 7original.vmx"  
+D:\vmware9\vmrun -T ws start "D:\vmimg\win7group\link-win7androiddev\Clone of Windows 7original.vmx"  
+
+
+D:\vmware9\vmrun -T ws start "I:\vmimg\centosdevelop\centosdevelop.vmx"  
+
+
+D:\vmware9\vmrun -T ws start "I:\vmimg\xeonbigmongodb\mybigmongdb.vmx"  
+
+
+
+
