66f63c5eed7a9cdc2cb47ae9cab54ec7ba3551e7
Damon <610576334@qq.com>
2020-03-18 22:04:55 +0800

    Create suspend.bat

---
+++ esxi/hp/vmcmd/suspend.bat
@@ -0,0 +1,4 @@
+D:\vmware\vmrun -T ws suspend  "G:\vmwareimg\xpgroup\linked-baoxian\Clone of shouhuiorigianlxp.vmx" 
+D:\vmware\vmrun -T ws suspend  "G:\vmwareimg\xpgroup\linked-baoxian3\baoxian3.vmx" 
+D:\vmware\vmrun -T ws suspend  "G:\vmwareimg\xpgroup\linked-chexian\Clone of shouhuiorigianlxp.vmx" 
+D:\vmware\vmrun -T ws suspend  "G:\vmwareimg\xpgroup\linked-chexian3\Clone of shouhuiorigianlxp.vmx" 
