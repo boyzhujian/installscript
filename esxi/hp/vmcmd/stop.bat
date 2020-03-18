e10e997bcbad9242965af6a5a27c50e750940cbd
Damon <610576334@qq.com>
2020-03-18 22:04:32 +0800

    Create stop.bat

---
+++ esxi/hp/vmcmd/stop.bat
@@ -0,0 +1,14 @@
+G:\vmware10\vmrun -T ws stop  "G:\vmwareimg\shouhuixpgroup\linked-1\Clone of shouhuiorigianlxp.vmx"  soft
+G:\vmware10\vmrun -T ws stop  "G:\vmwareimg\shouhuixpgroup\linked-shouhui2\Clone of shouhuiorigianlxp.vmx" soft
+G:\vmware10\vmrun -T ws stop "G:\vmwareimg\shouhuixpgroup\linked-shouhui11\Clone of shouhuiorigianlxp.vmx"  soft
+G:\vmware10\vmrun -T ws stop  "G:\vmwareimg\shouhuixpgroup\linked-shouhui12\Clone of shouhuiorigianlxp.vmx"  soft
+G:\vmware10\vmrun -T ws stop  "G:\vmwareimg\shouhuixpgroup\linked-shouhui13\shouhui13.vmx"  soft
+G:\vmware10\vmrun -T ws stop  "G:\vmwareimg\shouhuixpgroup\linked-shouhui14\shouhui14.vmx" soft
+
+
+
+
+G:\vmware10\vmrun -T ws stop  "G:\vmwareimg\xpgroup\linked-baoxian\Clone of shouhuiorigianlxp.vmx"  soft
+G:\vmware10\vmrun -T ws stop  "G:\vmwareimg\xpgroup\linked-baoxian3\baoxian3.vmx" soft
+G:\vmware10\vmrun -T ws stop "G:\vmwareimg\xpgroup\linked-chexian\Clone of shouhuiorigianlxp.vmx"  soft
+G:\vmware10\vmrun -T ws stop  "G:\vmwareimg\xpgroup\linked-chexian3\Clone of shouhuiorigianlxp.vmx"  soft
