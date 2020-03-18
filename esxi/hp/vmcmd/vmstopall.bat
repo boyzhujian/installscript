ffa5b8fb2dde0e952201707a217f69ed10fb293b
Damon <610576334@qq.com>
2020-03-18 22:06:41 +0800

    Create vmstopall.bat

---
+++ esxi/hp/vmcmd/vmstopall.bat
@@ -0,0 +1,38 @@
+
+
+
+D:\vmware9\vmrun -T ws stop "D:\vmimg\win7group\link-main\Clone of Windows 7original.vmx" soft
+D:\vmware9\vmrun -T ws stop "D:\vmimg\win7group\link-win7androiddev\Clone of Windows 7original.vmx" soft
+
+
+D:\vmware9\vmrun -T ws stop "D:\vmimg\win7group\link-shouhuimainwin7\Clone of Windows 7.vmx" soft
+D:\vmware9\vmrun -T ws stop "D:\vmimg\xpgroup\linked-1\Clone of shouhuiorigianlxp.vmx" soft
+D:\vmware9\vmrun -T ws stop "D:\vmimg\xpgroup\linked-shouhui2\Clone of shouhuiorigianlxp.vmx" soft
+D:\vmware9\vmrun -T ws stop "D:\vmimg\xpgroup\linked-shouhui3\Clone of shouhuiorigianlxp.vmx" soft
+D:\vmware9\vmrun -T ws stop "D:\vmimg\xpgroup\linked-shouhui11\Clone of shouhuiorigianlxp.vmx" soft
+D:\vmware9\vmrun -T ws stop "D:\vmimg\xpgroup\linked-shouhui12\Clone of shouhuiorigianlxp.vmx" soft
+D:\vmware9\vmrun -T ws stop "D:\vmimg\xpgroup\linked-shouhui13\shouhui13.vmx" soft
+D:\vmware9\vmrun -T ws stop "D:\vmimg\xpgroup\linked-shouhui14\shouhui14.vmx" soft
+
+
+D:\vmware9\vmrun -T ws stop "I:\vmimg\win7-32bit\windows 7.vmx" soft
+
+D:\vmware9\vmrun -T ws stop "D:\vmimg\mainwin7\windows 7.vmx" soft
+
+
+D:\vmware9\vmrun -T ws stop "F:\vmimage\centosdevelop\centosdevelop.vmx" soft
+
+
+D:\vmware9\vmrun -T ws stop "I:\vmimg\centosdevelop\centosdevelop.vmx"  
+
+
+D:\vmware9\vmrun -T ws stop "E:\vmimg\server2008r2\Windows Server 2008 R2 x64.vmx" soft
+
+
+
+D:\vmware9\vmrun -T ws stop "E:\vmimg\iredmailcentos\iredmail.vmx" soft
+
+
+D:\vmware9\vmrun -T ws stop "I:\vmimg\xeonbigmongodb\mybigmongdb.vmx"   soft
+
+
