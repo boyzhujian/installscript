51a84a0a2cccbad451224bcf0214f351df004257
Damon <610576334@qq.com>
2020-10-02 21:04:41 +0800

    Create install.sh

---
+++ foreman/install.sh
@@ -0,0 +1,11 @@
+#https://theforeman.org/manuals/1.7/index.html#3.InstallingForeman
+
+yum -y install http://yum.theforeman.org/releases/1.7/el7/x86_64/foreman-release.rpm
+yum -y install foreman-installer
+
+
+foreman-installer
+#foreman-installer -i
+#/etc/foreman/foreman-installer.yaml
+
+
